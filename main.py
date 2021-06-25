390c230ac2423d2322a88d1107b480e658593cf8
amitkumardatta <84867776+amitkumardatta@users.noreply.github.com>
2021-06-25 18:17:22 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import calendar
+try:
+    y= int(input())
+    if 1>y or y>9999:
+        print("please choose another year")
+    else:
+        for i in range (12):
+            print(calendar.month(y,i+1))
+except:
+    print("please choose a year of calendar")
+
+print ("Created by Amit Kumar Datta")
+#amitkumardatta
